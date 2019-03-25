5413582cb04eb3946b077fb6b310140bcc3cf509
Kazumasa KUMAMOTO <kumamotone@gmail.com>
2019-03-25 20:19:36 +0900

    課題 2.3

--- app/src/main/java/com/example/aacstudy/MainActivity.kt
+++ app/src/main/java/com/example/aacstudy/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.aacstudy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +13,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val lifecycleObserver = object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            fun onCreate(source: LifecycleOwner) {
+                println("ON_CREATE : ${source.lifecycle.currentState.name}")
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+            fun onAny(source: LifecycleOwner) {
+                println("ON_ANY : ${source.lifecycle.currentState.name}")
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+            fun onStop(source: LifecycleOwner) {
+                println("ON_STOP : ${source.lifecycle.currentState.name}")
+                // source.lifecycle.removeObserver(this)
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+            fun onDestroy(source: LifecycleOwner) {
+                println("ON_DESTROY : ${source.lifecycle.currentState.name}")
+            }
+        }
+        lifecycle.addObserver(lifecycleObserver)
         println("onCreate : ${lifecycle.currentState.name}")
     }
 
