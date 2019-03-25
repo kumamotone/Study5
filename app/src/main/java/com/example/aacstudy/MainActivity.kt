78480ddaf6d713b760bd08c0ac0511754a32b0cf
Kazumasa KUMAMOTO <kumamotone@gmail.com>
2019-03-25 19:56:39 +0900

    :tada

---
+++ app/src/main/java/com/example/aacstudy/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aacstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
